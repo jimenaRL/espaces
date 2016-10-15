f96309a35c93840bde6ca79f2ad0ff91108930a7
jimena <jimena@melix.net>
2016-10-15 11:21:16 +0200

    cleaned green_fn

--- ir/green_fn.py
+++ ir/green_fn.py
@@ -9,42 +9,15 @@
 from tqdm import tqdm
 import numpy as np
 
-import theano 
-import theano.tensor as T
-
-
-def compute_green_fn_theano(c,nu,eigen_vals,duration,sampling_rate):
-
-    c  = np.float64(c)
-    nu = np.float64(nu)
-
-    time_step  = 1/np.float64(sampling_rate)
-    dur_points = np.int(duration/time_step)
-    t_discret  = np.linspace(0,duration,dur_points).reshape((1,dur_points))
-    X = T.constant(t_discret)
-
-    values = np.array([e['value'] for e in  eigen_vals]).astype('float32').reshape(len(eigen_vals),1)
-
-    multiplicities = np.array([e['multiplicity'] for e in  eigen_vals]).astype('float32').reshape(len(eigen_vals),1)
-    V = T.constant(values)
-
-    EXP = T.sqrt(V*c*c-V*V*nu*nu)
-    EXP_X = EXP*X
-
-    theano_green_fn_0 = ( multiplicities * (T.exp( (-1) * nu * V*X ) * T.cos( T.sqrt(V) * c * T.sqrt(1-(V*nu*nu/c*c) ) * X ) ) ).sum(axis=0)
-
-    return theano_green_fn_0.eval()
-
-
-def compute_green_fn(c,nu,eigen_vals,duration,sampling_rate,sym=False):
+def compute_green_fn(c,nu,eigen_vals,duration,sampling_rate):
     """ Returns the Green function at x=0 of the wave equation 
         in a manifold M defined by its eigen-values.
 
         c               : [float] sound velocity
         nu              : [float] kinematic viscosity
-        eigen_vals      : TO DO 
+        eigen_vals      : [list of dict] eigen-values and their multiplicities of a manifold M 
         duration        : [float] duration of the Green function in seconds
-        sampling_rate   : [int]   sampling rate of the output sound
+        sampling_rate   : [int] sampling rate of the output
     """
 
     # convert to correct type for precaution
--- ir/test.py
+++ ir/test.py
@@ -1,53 +1,61 @@
-import os
+# import os
 
-from utils import  open_osx
+# from utils import  open_osx
 
-from convolutions import convolve_signals
-from signals import Signal
-from espaces import espaces
+# from convolutions import convolve_signals
+# from signals import Signal
+# from espaces import espaces
 
-# add to python path
-ESPACES_PROJECT = os.environ['ESPACES_PROJECT']
+# # add to python path
+# ESPACES_PROJECT = os.environ['ESPACES_PROJECT']
 
-def test(save=False,open=False):
+def test_eigenvalues():
+    from eigenvalues import eigenvalues
+    eigenvalues({'space':'e3'  , 'F':[0.1,0.1,0.1], 'j_max':2})
+    eigenvalues({'space':'s3'  , 'F':[0.1], 'c':1})
+    eigenvalues({'space':'h2e1', 'F':[0.1], 'kind':3})
+    eigenvalues({'space':'s2e1', 'F':[0.1,0.1], 'unused_kwarg':'unused_kwarg'})
 
-    # ordinary impulse reponse 
-    path_ir = os.path.join(ESPACES_PROJECT,'data','examples','ir.wav')
-    sig_ir = Signal(path_ir,mono=True)
+def test_compute_green_fn():
+    from green_fn import compute_green_fn
+    compute_green_fn(c=1, nu=1,  eigen_vals=[{'multiplicity':1, 'value':1}], duration=0.1,sampling_rate=8000)
 
-    # audio example
-    path_ex = os.path.join(ESPACES_PROJECT,'data','examples','speech.wav')
-    sig_ex = Signal(path_ex,mono=True)
+# def test(save=False,open=False):
 
-    # convolved signal
-    mode = 'full' # 'full' or 'valid' or 'same'
-    kind = 'ola' # 'ola' or 'ss'
-    sig_cv = convolve_signals(sig_ex, sig_ir, mode, kind)
+#     # ordinary impulse reponse 
+#     path_ir = os.path.join(ESPACES_PROJECT,'data','examples','ir.wav')
+#     sig_ir = Signal(path_ir,mono=True)
 
-    # save audio and image
-    for name in ['ex','ir','cv']:
-        if save:
-            save_path = os.path.join(ESPACES_PROJECT,'data','examples','to_erase_'+name+'_'+kind)
-            save_au = save_path+'_'+mode+'.wav'
-            save_im = save_path+'_'+mode+'.png'
-            sig = locals()['sig_'+name]
-            sig.write(save_au)
-            sig.save_image(save_im, title=name+' '+mode+' '+kind)
-        if open:
-            open_osx(save_au,save_im)
+#     # audio example
+#     path_ex = os.path.join(ESPACES_PROJECT,'data','examples','speech.wav')
+#     sig_ex = Signal(path_ex,mono=True)
 
-def profiling():
-    import time
-    import sys
-    from cProfile import Profile
-    from pstats import Stats
+#     # convolved signal
+#     mode = 'full' # 'full' or 'valid' or 'same'
+#     kind = 'ola' # 'ola' or 'ss'
+#     sig_cv = convolve_signals(sig_ex, sig_ir, mode, kind)
 
-    p = Profile()
-    p.runcall(test)
-    stats = Stats(p, stream=sys.stdout)
-    stats.sort_stats('time')
-    stats.print_stats(10)
+#     # save audio and image
+#     for name in ['ex','ir','cv']:
+#         if save:
+#             save_path = os.path.join(ESPACES_PROJECT,'data','examples','to_erase_'+name+'_'+kind)
+#             save_au = save_path+'_'+mode+'.wav'
+#             save_im = save_path+'_'+mode+'.png'
+#             sig = locals()['sig_'+name]
+#             sig.write(save_au)
+#             sig.save_image(save_im, title=name+' '+mode+' '+kind)
+#         if open:
+#             open_osx(save_au,save_im)
 
-if __name__:
-    test(save=True,open=True)
-    # profiling()
+# def profiling():
+#     import time
+#     import sys
+#     from cProfile import Profile
+#     from pstats import Stats
+
+#     p = Profile()
+#     p.runcall(test)
+#     stats = Stats(p, stream=sys.stdout)
+#     stats.sort_stats('time')
+#     stats.print_stats(10)
+
