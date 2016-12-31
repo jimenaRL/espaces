546d8c4a903e3713737bac21be949545fc0a106c
jimena <jimena@melix.net>
2016-12-31 18:10:45 +0100

    Add error for missing SSL files.

--- website/server/server.py
+++ website/server/server.py
@@ -16,6 +16,9 @@ crt_path = os.path.join(parent_dir, 'ssl', 'nginx.crt')
 key_path = os.path.join(parent_dir, 'ssl', 'nginx.key')
 context = (crt_path, key_path)
 
+if not os.path.exists(crt_path) or not os.path.exists(crt_path):
+    raise ValueError("Missing files for SSL context %s and/or %s." % (crt_path, key_path))
+
 @app.route('/', methods=['GET'])
 def main():
     return redirect(url_for('static', filename='index.html'))
