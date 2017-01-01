26215cc421154221eaafc453374e3cee71d838a7
jimena <jimena@melix.net>
2017-01-01 19:35:16 +0100

    Website refactoring

--- website/server/static/main.js
+++ website/server/static/main.js
@@ -1,18 +1,21 @@
 var auralizr = new Auralizr();
 
 var data = {
-    'h2e1' : {'ir':'content/ir/ecm_20160719/h2e1.wav', 'image':'content/images/background/h2e1.png'},
-    's2e1' : {'ir':'content/ir/ecm_20160719/s2e1.wav', 'image':'content/images/background/s2e1.png'},
-    'e3'   : {'ir':'content/ir/ecm_20160719/e3.wav',   'image':'content/images/background/e3.png'},
-    's3'   : {'ir':'content/ir/ecm_20160719/s3.wav',   'image':'content/images/background/s3.png'},
+    'h2e1' : {'ir':'content/ir/ecm_20160719/h2e1.wav', 'image':'content/images/main/h2e1.png'},
+    's2e1' : {'ir':'content/ir/ecm_20160719/s2e1.wav', 'image':'content/images/main/s2e1.png'},
+    'e3'   : {'ir':'content/ir/ecm_20160719/e3.wav',   'image':'content/images/main/e3.png'},
+    's3'   : {'ir':'content/ir/ecm_20160719/s3.wav',   'image':'content/images/main/s3.png'},
 };
+
+var play  = '▶';
+var pause = '❚❚';
 
 if (auralizr.userMediaSupport){
     var onAuralizrLoad = function (key){
             var element = document.getElementsByClassName(key)[0];
             if (element) {
                 enableClickFunctionality(data[key]['image'],element,key);
-                element.innerHTML = '▶';
+                element.innerHTML = play;
             }
         };
     for (var key in data){
@@ -24,23 +27,23 @@ function resetAllSpans() {
     var allPlaces =  [].slice.call(document.getElementsByClassName('place'));
     allPlaces.forEach(function(element) {
         element.classList.remove('enabled');
-        if (element.innerHTML === '❚❚')
-            element.innerHTML = '▶';
+        if (element.innerHTML === pause)
+            element.innerHTML = play;
     });
 }
 
 function enableThisSpan(element){
     element.classList.add('enabled');
-    element.innerHTML = '❚❚';
+    element.innerHTML = pause;
 }
 
 function enableClickFunctionality(url_str,element,key){
     element.addEventListener('click',function(event){
 
         console.log('load image from '+url_str)
-        document.body.style.backgroundImage = "url('"+url_str+"')"
+        document.getElementById('img_main').src = url_str
 
-        if (element.innerHTML === '▶'){
+        if (element.innerHTML === play){
             resetAllSpans();
             auralizr.use(this.id);
             if (!auralizr.isRunning){
