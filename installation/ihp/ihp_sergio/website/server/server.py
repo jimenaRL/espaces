7a7bcdbea50739354710bb3745b30d69b93195dd
jimena <jimena@melix.net>
2017-03-25 16:19:37 +0100

    remove website from root dir

--- installation/ihp/ihp_sergio/website/server/server.py
+++ installation/ihp/ihp_sergio/website/server/server.py
@@ -6,22 +6,26 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)
 
-PORT = 8888
+PORT = 80
 HOST = "192.168.0.48"
-DEBUG = True
+DEBUG = False
 
 # Use SSH. Set a context with crt and key files and path it to app.run()
-parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                          os.path.pardir))
 crt_path = os.path.join(parent_dir, 'ssl', 'nginx.crt')
 key_path = os.path.join(parent_dir, 'ssl', 'nginx.key')
 context = (crt_path, key_path)
 
 if not os.path.exists(crt_path) or not os.path.exists(crt_path):
-    raise ValueError("Missing files for SSL context %s and/or %s." % (crt_path, key_path))
+    raise ValueError("Missing files for SSL context %s and/or %s."
+                     % (crt_path, key_path))
+
 
 @app.route('/', methods=['GET'])
 def main():
     return redirect(url_for('static', filename='index.html'))
 
+
 if __name__ == "__main__":
-     app.run(HOST,PORT,DEBUG,ssl_context=context)
+    app.run(HOST, PORT, DEBUG, ssl_context=context)
