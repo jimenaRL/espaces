01ab46c196543b3eb53844f29b50c0a6d913ff92
jimena <jimena@melix.net>
2017-11-20 17:34:52 -0600

    PEP8

--- ir/compute.py
+++ ir/compute.py
@@ -1,60 +1,67 @@
 from espaces import espaces
+from ee_utils import cartesian
+
 
 def compute_s2e1():
 
-    ir_params = {  'ev_params'      : {'space':'s2e1', 'c':3.4e2, 'j_max':1000},
-                   'duration'       : 15.0,
-                   'nu'             : 1.7e-5 * .5e3,
-                   'sampling_rate'  : 8000,
-                }
+    ir_params = {
+        'ev_params': {'space': 's2e1', 'c': 3.4e2, 'j_max': 1000},
+        'duration': 15.0,
+        'nu': 1.7e-5 * .5e3,
+        'sampling_rate': 8000,
+    }
 
-    f = [0.001,0.01,0.1,1,10,100,1000,10000]
-    for F in cartesian([f,f]):
+    f = [0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000]
+    for F in cartesian([f, f]):
         print "\n--------%s--------\n" % ir_params
         ir_params['ev_params']['F'] = F
         espaces(ir_params)
 
+
 def compute_h2e1():
 
-    ir_params = {  'ev_params'      : {'space':'h2e1', 'c':3.4e2, 'j_max':1000},
-                   'duration'       : 15.0,
-                   'nu'             : 1.7e-5 * .5e3,
-                   'sampling_rate'  : 8000,
-                }
+    ir_params = {
+        'ev_params': {'space': 'h2e1', 'c': 3.4e2, 'j_max': 1000},
+        'duration': 15.0,
+        'nu': 1.7e-5 * .5e3,
+        'sampling_rate': 8000,
+    }
 
-    f = [0.001,0.01,0.1,1,10,100,1000,10000]
+    f = [0.001, 0.01, 0.1, 1, 10, 100, 1000, 10000]
     for F in f:
         print "\n--------%s--------\n" % ir_params
         ir_params['ev_params']['F'] = [F]
         espaces(ir_params)
 
+
 def compute_e3():
 
-    ir_params = {  'ev_params'      : {'space':'e3', 'c':3.4e2, 'j_max':15},
-                   'duration'       : 15.0,
-                   'nu'             : 1.7e-5,
-                   'sampling_rate'  : 8000,
-                }
+    ir_params = {
+        'ev_params': {'space': 'e3', 'c': 3.4e2, 'j_max': 15},
+        'duration': 15.0,
+        'nu': 1.7e-5,
+        'sampling_rate': 8000,
+    }
 
-    space = 'n_torus'
-
-    f = [0.001,0.01,0.1,1,10,100]
+    f = [0.001, 0.01, 0.1, 1, 10, 100]
 
     # for F in ( [100,100,100], [50,50,50], [100,50,75]):
-    for F in cartesian([f,f,f]):
+    for F in cartesian([f, f, f]):
         print "\n--------%s--------\n" % ir_params
         ir_params['ev_params']['F'] = F
         espaces(ir_params)
 
+
 def compute_s3():
 
-    ir_params = {  'ev_params'      : {'space':'s3', 'c':3.4e2, 'j_max':200},
-                   'duration'       : 15.0,
-                   'nu'             : 1.7e-5 * .5e4,
-                   'sampling_rate'  : 8000,
-                }
+    ir_params = {
+        'ev_params': {'space': 's3', 'c': 3.4e2, 'j_max': 200},
+        'duration': 15.0,
+        'nu': 1.7e-5 * .5e4,
+        'sampling_rate': 8000,
+    }
 
-    f1 = [0.001,0.01,0.1,1,10,100]
+    f1 = [0.001, 0.01, 0.1, 1, 10, 100]
     for F in f1:
         print "\n--------%s--------\n" % ir_params
         ir_params['ev_params']['F'] = [F]
--- ir/ee_utils.py
+++ ir/ee_utils.py
@@ -6,12 +6,16 @@
 #                                                                            */
 # -------------------------------------------------------------------------- */
 
-import os, sys
+import os
 from datetime import date
 
 import numpy as np
 
-ESPACES_PROJECT = os.path.realpath(os.path.join(os.path.abspath(__file__), os.pardir, os.pardir,os.pardir))
+ESPACES_PROJECT = os.path.realpath(os.path.join(os.path.abspath(__file__),
+                                                os.pardir,
+                                                os.pardir,
+                                                os.pardir))
+
 
 def cartesian(arrays,  out=None):
     """
@@ -44,20 +48,22 @@ def cartesian(arrays,  out=None):
             out[j*m:(j+1)*m, 1:] = out[0:m, 1:]
     return out
 
-def set_folders(space,simple_paths=False):
+
+def set_folders(space, simple_paths=False):
     """ """
 
     folders = {}
 
     if simple_paths:
-        results_path = os.path.join(ESPACES_PROJECT,'data')
-        folders['audio'] = os.path.join(results_path,space)
+        results_path = os.path.join(ESPACES_PROJECT, 'data')
+        folders['audio'] = os.path.join(results_path, space)
 
     else:
-        results_path = os.path.join(ESPACES_PROJECT,'data','dates',date.today().isoformat())
-        folders['evs'] = os.path.join(results_path,space,'eigenvalues')
-        folders['image'] = os.path.join(results_path,space,'green_fn','images')
-        folders['audio'] = os.path.join(results_path,space,'green_fn','audio')
+        results_path = os.path.join(ESPACES_PROJECT, 'data', 'dates',
+                                    date.today().isoformat())
+        folders['evs'] = os.path.join(results_path, space, 'eigenvalues')
+        folders['image'] = os.path.join(results_path, space, 'green_fn', 'images')
+        folders['audio'] = os.path.join(results_path, space, 'green_fn', 'audio')
 
     for folder in folders.values():
         if not os.path.exists(folder):
@@ -69,18 +75,17 @@ def set_folders(space,simple_paths=False):
 def get_paths(**kwargs):
     """ """
 
-    kwargs['F'] = '_'.join([str(f) for f in kwargs.get('F',[])])
-    name  = '_'.join(["%s_%s" % (k,v) for k,v in kwargs.iteritems()])
-    folders = set_folders(kwargs.get('space','unknown'))
+    kwargs['F'] = '_'.join([str(f) for f in kwargs.get('F', [])])
+    name = '_'.join(["%s_%s" % (k, v) for k, v in kwargs.iteritems()])
+    folders = set_folders(kwargs.get('space', 'unknown'))
 
     paths = {}
 
-    paths['audio'] = os.path.join(folders['audio'],name+'.wav')
+    paths['audio'] = os.path.join(folders['audio'], name+'.wav')
 
     if 'evs' in folders:
-        paths['evs'] = os.path.join(folders['evs'],name+'.tsv')
+        paths['evs'] = os.path.join(folders['evs'], name+'.tsv')
     if 'image' in folders:
-        paths['image'] = os.path.join(folders['image'],name+'.png')
+        paths['image'] = os.path.join(folders['image'], name+'.png')
 
     return paths
-
