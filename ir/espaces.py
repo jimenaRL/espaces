84ded7a29cb2001aa6b729f696f7d2adb7be2905
jimena <jimena@melix.net>
2017-11-20 16:12:06 -0600

    PEP8

--- ir/espaces.py
+++ ir/espaces.py
@@ -8,33 +8,31 @@
 
 # All units in International System of Units (SI)
 
-import os, sys, csv, time
-import itertools, copy
+import os
+import csv
+import copy
 
-# from joblib import Parallel, delayed
-
-import numpy as np
-
-from ee_utils import get_paths, cartesian
+from ee_utils import get_paths
 from impulse_reponse import get_ir
+
 
 class EspaceClient(object):
 
     def __init__(self):
         pass
 
-    def handle_request(self,ir_params):
+    def handle_request(self, ir_params):
         saved_audio_path = espaces(ir_params)
-        return {'saved_audio_path':saved_audio_path}
+        return {'saved_audio_path': saved_audio_path}
 
 
 def espaces(ir_params):
 
     # set save paths results
     path_kwargs = copy.deepcopy(ir_params['ev_params'])
-    path_kwargs.update({'duration' : ir_params['duration'],
-                        'nu'       : ir_params['nu'] })
-    paths  = get_paths(**path_kwargs)
+    path_kwargs.update({'duration': ir_params['duration'],
+                        'nu': ir_params['nu']})
+    paths = get_paths(**path_kwargs)
 
     au_path = paths.get('audio')
 
@@ -48,9 +46,9 @@ def espaces(ir_params):
 
         # save eigenvalues
         if 'evs' in paths:
-            with open(paths['evs'],'w') as f:
-                writer = csv.writer(f,delimiter='\t')
-                writer.writerows([ (item['value'],item['multiplicity']) for item in eigen_vals])
+            with open(paths['evs'], 'w') as f:
+                writer = csv.writer(f, delimiter='\t')
+                writer.writerows([(item['value'], item['multiplicity']) for item in eigen_vals])
 
         # save ir image
         if 'image' in paths:
@@ -60,6 +58,3 @@ def espaces(ir_params):
         print "Audio file found at %s." % au_path
 
     return au_path
-
-
-
